88fc250fccd4b395918f6aa2fc2f71372d3dc914
previzdaniel <dani.2000.2x@gmail.com>
2020-09-08 11:16:40 +0200

    számítógép választ

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Játékprogram (kő, papír, olló)
 
 ## játékmenet 1.0
-- []A számítógép választ egy lehetőséget
+- [x]A számítógép választ egy lehetőséget
 - []A felhasználó megad egy lehetőséget
 
 - Nyerés:
--- feladat09.08/Program.cs
+++ feladat09.08/Program.cs
@@ -4,12 +4,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace feladat09._08
+namespace feladat09_08
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Random  veletlen= new Random();
+            string[] lehetoseg = new string[] {"kő", "papír", "olló"};
+
+            int gepValasz = veletlen.Next(0, 2);
+
+            Console.WriteLine("A gép választása: {0}",lehetoseg[gepValasz]);
+
+            Console.ReadKey();
         }
     }
 }
